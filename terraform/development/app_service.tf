951f39ec13ece70ef3d57651f136b7424955a82e
Cris92 <caiazzocristian92@live.com>
2024-08-12 23:43:11 +0200

    fix(infrastracture): i've added some paramters to correctly initialize the pytyhon env on webapp

--- terraform/development/app_service.tf
+++ terraform/development/app_service.tf
@@ -17,14 +17,15 @@ resource "azurerm_linux_web_app" "app" {
 
   site_config {
     app_command_line = "/home/site/wwwroot/hotel_pegaso/app_service_config.sh"
+    linux_fx_version = "PYTHON|3.9"
   }
-
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "DB_HOST"                  = azurerm_postgresql_server.postgres_server.fqdn
-    "DB_NAME"                  = azurerm_postgresql_database.postgres_db.name
-    "DB_USER"                  = "${azurerm_postgresql_server.postgres_server.administrator_login}@${azurerm_postgresql_server.postgres_server.name}"
-    "DB_PASSWORD"              = azurerm_key_vault_secret.db_password.value
-    "SECRET_KEY"               = "django-insecure-de^(3m@7^j+4vix#p&1vj)(3h_tr(h+5d%uofit*g8zb9ecc6a"
+    "WEBSITE_RUN_FROM_PACKAGE"        = "1"
+    "PYTHON_ENABLE_WORKER_EXTENSIONS" = "true"
+    "DB_HOST"                         = azurerm_postgresql_server.postgres_server.fqdn
+    "DB_NAME"                         = azurerm_postgresql_database.postgres_db.name
+    "DB_USER"                         = "${azurerm_postgresql_server.postgres_server.administrator_login}@${azurerm_postgresql_server.postgres_server.name}"
+    "DB_PASSWORD"                     = azurerm_key_vault_secret.db_password.value
+    "SECRET_KEY"                      = "django-insecure-de^(3m@7^j+4vix#p&1vj)(3h_tr(h+5d%uofit*g8zb9ecc6a"
   }
 }
