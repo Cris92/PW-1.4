49a3ca0446d8491c7a757836b722d6cce565691d
Cris92 <caiazzocristian92@live.com>
2024-08-13 16:23:50 +0200

    fix(booking-app): i've  changed db settings

--- hotel_pegaso/hotel_pegaso/settings.py
+++ hotel_pegaso/hotel_pegaso/settings.py
@@ -78,28 +78,28 @@ WSGI_APPLICATION = 'hotel_pegaso.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),          
-#         'USER': os.getenv('DB_USER'),          
-#         'PASSWORD': os.getenv('DB_PASSWORD'),  
-#         'HOST': os.getenv('DB_HOST'),          
-#         'PORT': '5432', 
-#          'OPTIONS': {
-#             'sslmode': 'require',
-#         },                           
-#     }
-# }
-#ALLOWED_HOSTS = ['.azurewebsites.net', 'as-pegaso-dev-westeu-001.scm.azurewebsites.net']
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),          
+        'USER': os.getenv('DB_USER'),          
+        'PASSWORD': os.getenv('DB_PASSWORD'),  
+        'HOST': os.getenv('DB_HOST'),          
+        'PORT': '5432', 
+         'OPTIONS': {
+            'sslmode': 'require',
+        },                           
     }
 }
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.azurewebsites.net', 'as-pegaso-dev-westeu-001.scm.azurewebsites.net']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# ALLOWED_HOSTS = []
 
 
 # Password validation
