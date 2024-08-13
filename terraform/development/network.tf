63cdfbf3f74f06272b0c0f554346624fb3222bbf
Cris92 <caiazzocristian92@live.com>
2024-08-13 16:12:08 +0200

    fix(infrastracture): i've fixed security rules

--- terraform/development/network.tf
+++ terraform/development/network.tf
@@ -31,13 +31,24 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "AllowHAppInbound"
+    name                       = "AllowHttp"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80,8000,443,8443"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "AllowHHttps"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
