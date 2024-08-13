e83c170d56beba1c8284d9856ba129a617924ad1
Cris92 <caiazzocristian92@live.com>
2024-08-13 11:59:15 +0200

    fix(booking-app): i've added whitenoise for static file serving

--- hotel_pegaso/hotel_pegaso/settings.py
+++ hotel_pegaso/hotel_pegaso/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'hotel_pegaso.urls'
 
 TEMPLATES = [
