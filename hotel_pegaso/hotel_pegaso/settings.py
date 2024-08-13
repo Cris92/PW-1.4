355c163fd88f17cd325114a9e59a2f6741a3d767
Cris92 <caiazzocristian92@live.com>
2024-08-13 10:49:38 +0200

    feat(booking-app): i've added ssl to settings.py to connect to psql and added firewall rules to app

--- hotel_pegaso/hotel_pegaso/settings.py
+++ hotel_pegaso/hotel_pegaso/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'hotel_pegaso.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,9 +85,13 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),          
         'PASSWORD': os.getenv('DB_PASSWORD'),  
         'HOST': os.getenv('DB_HOST'),          
-        'PORT': '5432',                        
+        'PORT': '5432', 
+         'OPTIONS': {
+            'sslmode': 'require',
+        },                           
     }
 }
+
 
 
 # Password validation
